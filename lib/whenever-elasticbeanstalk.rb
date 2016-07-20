9c66c32f0c58edef50cc9e4f7d0997b95603da21
Joel Courtney <jcourtney@cozero.com.au>
2016-07-20 11:41:50 +1000

    Update bugfix bundle exec

--- lib/whenever-elasticbeanstalk.rb
+++ lib/whenever-elasticbeanstalk.rb
@@ -118,7 +118,7 @@ module Whenever
         ec2_instance = @ec2_resource.instance(@instance_id)
         env_name_tag = ec2_instance.tags.find { |t| t.key == ENVIRONMENT_NAME_TAG }
         File.open(filename, 'w') { |f| f.write(env_name_tag.value) }
-        env_name
+        env_name_tag.value
       end
     end
 
